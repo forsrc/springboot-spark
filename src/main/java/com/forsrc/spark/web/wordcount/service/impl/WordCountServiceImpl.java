fd219721cb7d755bc99920937ea2fcfc590ffa1a
yes <forsrc@gmail.com>
2018-01-09 17:48:18 +0900

    Update WordCountServiceImpl.java

--- src/main/java/com/forsrc/spark/web/wordcount/service/impl/WordCountServiceImpl.java
+++ src/main/java/com/forsrc/spark/web/wordcount/service/impl/WordCountServiceImpl.java
@@ -20,20 +20,63 @@ import com.forsrc.spark.web.wordcount.service.WordCountService;
 
 import scala.Tuple2;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.RelationalGroupedDataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+
 @Service
 public class WordCountServiceImpl implements WordCountService {
 
     @Autowired
     private transient JavaSparkContext javaSparkContext;
+    
+    @Autowired
+    private SparkSession sparkSession;
 
     private static final Pattern SPACE = Pattern.compile(" ");
 
     @Override
     public Map<String, Integer> wordCount(String filename) {
+        return wordCount2(filename);
+    }
+
+    public Map<String, Integer> wordCount2(String filename) {
+        final Map<String, Integer> map = new HashMap<>();
+ 
+        Dataset<String> df = sparkSession.read()
+                                         .text(filename)
+                                         .as(Encoders.STRING());
+        Dataset<String> words = df.flatMap(word -> {
+                                            System.out.println("--> " + word);
+                                            return Arrays.asList(SPACE.split(word)).iterator();
+                                        }, Encoders.STRING())
+                                  .filter(word -> !word.isEmpty())
+                                  .coalesce(1);
+
+        words.printSchema();
+
+        Dataset<Row> t = words.groupBy("value")
+                              .count()
+                              .toDF("word", "count");
+
+        t = t.sort(functions.desc("count"));
+ 
+        //t.toJavaRDD().saveAsTextFile("wordcount.out");
+
+        t.toJavaRDD()
+         .collect()
+         .forEach(i -> map.put(i.getString(0), (int)i.getLong(1)));
+        return map;
+    }
+
+    public Map<String, Integer> wordCount1(String filename) {
 
         Map<String, Integer> map = new HashMap<>();
         JavaRDD<String> lines = javaSparkContext.textFile(filename);
-        JavaRDD<String> words = lines.flatMap(word -> Arrays.asList(SPACE.split(word)));
+        JavaRDD<String> words = lines.flatMap(word -> Arrays.asList(SPACE.split(word)).iterator());
         JavaPairRDD<String, Integer> counts = words
                 .mapToPair(word -> new Tuple2<>(word, 1))
                 .reduceByKey((Integer i1, Integer i2) -> (i1 + i2));
